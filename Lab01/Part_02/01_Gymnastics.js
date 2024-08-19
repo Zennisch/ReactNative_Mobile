eab556dafb872a2a4cddcc4f33573fa01db74368
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-19 18:51:47 +0900

    Add check winner and print results

--- Lab01/Part_02/01_Gymnastics.js
+++ Lab01/Part_02/01_Gymnastics.js
@@ -8,10 +8,30 @@ var data_2 = {
     koalas_scores: [23, 34, 27]
 }
 
-var calcAverage = function(scores) {
+var calcAverage = (scores) => {
     var sum = 0;
     for (var i = 0; i < scores.length; i++) {
         sum += scores[i];
     }
     return sum / scores.length;
 }
+
+var checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins > avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgDolphins < avgKoalas) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("It's a draw");
+    }
+}
+
+console.log("Data 1:");
+avgDolphins = calcAverage(data_1.dolphins_scores);
+avgKoalas = calcAverage(data_1.koalas_scores);
+checkWinner(avgDolphins, avgKoalas);
+
+console.log("Data 2:");
+avgDolphins = calcAverage(data_2.dolphins_scores);
+avgKoalas = calcAverage(data_2.koalas_scores);
+checkWinner(avgDolphins, avgKoalas);
