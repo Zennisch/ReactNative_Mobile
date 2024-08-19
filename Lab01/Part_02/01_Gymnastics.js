433c08eff699605c53c0740aa148f07c7fed0fc6
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-19 18:47:09 +0900

    Add calculate function

--- Lab01/Part_02/01_Gymnastics.js
+++ Lab01/Part_02/01_Gymnastics.js
@@ -7,3 +7,11 @@ var data_2 = {
     dolphins_scores: [85, 54, 41],
     koalas_scores: [23, 34, 27]
 }
+
+var calcAverage = function(scores) {
+    var sum = 0;
+    for (var i = 0; i < scores.length; i++) {
+        sum += scores[i];
+    }
+    return sum / scores.length;
+}
