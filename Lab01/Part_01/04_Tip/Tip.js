5abc1e73bd9a60e3eed849cc0fdb3996e2fc14a7
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-19 18:33:44 +0900

    Add print result

--- Lab01/Part_01/04_Tip/Tip.js
+++ Lab01/Part_01/04_Tip/Tip.js
@@ -13,3 +13,9 @@ var tip = (bill) => {
 var final_bill = (bill) => {
     return bill + tip(bill);
 }
+
+console.log(`The bill was ${bill_value_1}, the tip was ${tip(bill_value_1)}, and the total value ${final_bill(bill_value_1)}`);
+
+console.log(`The bill was ${bill_value_2}, the tip was ${tip(bill_value_2)}, and the total value ${final_bill(bill_value_2)}`);
+
+console.log(`The bill was ${bill_value_3}, the tip was ${tip(bill_value_3)}, and the total value ${final_bill(bill_value_3)}`);
