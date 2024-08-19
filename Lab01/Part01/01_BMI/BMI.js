4cf79e57f4fae3adc049e027df5bc4712bfed715
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-19 17:45:46 +0900

    Add tasks

--- Lab01/Part01/01_BMI/BMI.js
+++ Lab01/Part01/01_BMI/BMI.js
@@ -7,3 +7,16 @@ var john = {
     mass: 95,
     height: 1.88,
 };
+
+var calculateBMI = (person) => {
+    return person.mass / person.height ** 2;
+}
+
+var markBMI = calculateBMI(mark);
+var johnBMI = calculateBMI(john);
+
+var markHigherBMI = markBMI > johnBMI;
+
+console.log("Mark's BMI: " + markBMI);
+console.log("John's BMI: " + johnBMI);
+console.log("Is Mark's BMI higher than John's?: " + markHigherBMI);
