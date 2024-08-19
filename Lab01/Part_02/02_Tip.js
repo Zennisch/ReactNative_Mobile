52f08439aba519eff9ca02cf085e363d1b574586
Rei <snowy.sunny.dazing.us@gmail.com>
2024-08-19 18:59:34 +0900

    Add calculate tips function and print results

--- Lab01/Part_02/02_Tip.js
+++ Lab01/Part_02/02_Tip.js
@@ -12,3 +12,12 @@ var final_bill = (bill) => {
 
 bill_test = 100;
 console.log(`The bill was ${bill_test}, the tip was ${tip(bill_test)}, and the total value ${final_bill(bill_test)}`);
+
+var calcTips = (bills) => {
+    for (var i = 0; i < bills.length; i++) {
+        console.log(`The bill was ${bills[i]}, the tip was ${tip(bills[i])}, and the total value ${final_bill(bills[i])}`);
+    }
+}
+
+bill_datas = [125, 555, 44];
+calcTips(bill_datas);
