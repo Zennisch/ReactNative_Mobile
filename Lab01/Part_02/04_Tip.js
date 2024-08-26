a67235cf03c2b9adcfbd383b85cc926a9da82b33
Reina <snowy.sunny.dazing.us@gmail.com>
2024-08-26 13:24:52 +0900

    Add Tip

---
+++ Lab01/Part_02/04_Tip.js
@@ -0,0 +1,25 @@
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => {
+    if(bill >= 50 && bill <= 300){
+        return bill * 0.15;
+    } else {
+        return bill * 0.20;
+    }
+}
+
+const calcTips = (bills) => {
+    for (let i = 0; i < bills.length; i++) {
+        let bill = bills[i];
+        let tip = calcTip(bill);
+        tips.push(tip);
+        totals.push(tip + bill);
+    }
+}
+
+calcTips(bills);
+console.log(`Bills: ${bills.join(', ')}`);
+console.log(`Tips: ${tips.join(', ')}`);
+console.log(`Totals: ${totals.join(', ')}`);
