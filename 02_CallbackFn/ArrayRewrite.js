8ddfaf0e3b74d5ee29295944c222423717f07224
Reina <snowy.sunny.dazing.us@gmail.com>
2024-08-26 17:42:54 +0900

    Add myAt and Update myFilter

--- 02_CallbackFn/ArrayRewrite.js
+++ 02_CallbackFn/ArrayRewrite.js
@@ -1,5 +1,9 @@
 const numbers = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45];
 
+const myAt = (array, index) => {
+    return array[index];
+}
+
 const myFilter = (array, callback) => {
     const result = [];
 
@@ -12,9 +16,19 @@ const myFilter = (array, callback) => {
     return result;
 }
 
+Array.prototype.at = function(index) {
+    return myAt(this, index);
+}
 Array.prototype.filter = function(callback) {
     return myFilter(this, callback);
 }
 
-const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+console.log('\nMy At');
+const index = 3;
+const element = numbers.at(index);
+console.log(`Element at index ${index}: ${element}`);
+
+console.log('\nMy Filter');
+const callback = (number) => number > 20;
+const oddNumbers = numbers.filter(callback);
 console.log(`List of odd numbers: ${oddNumbers.join(', ')}`);
