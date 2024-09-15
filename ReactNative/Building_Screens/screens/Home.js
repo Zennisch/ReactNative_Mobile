6d6f326afeb6ccc32463dce079bd277e2f63b2bc
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-16 02:10:00 +0900

    Update

--- ReactNative/Building_Screens/screens/Home.js
+++ ReactNative/Building_Screens/screens/Home.js
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Image source={require("../assets/my_assets/circle.png")} />
+            <Text style={styles.title}>{"GROW YOUR BUSSINESS"}</Text>
+            <Text style={{ textAlign: "center" }}>
+                {
+                    "We will help you to grow your online business using\nonline server"
+                }
+            </Text>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "#00ccf9",
+        alignItems: "center",
+        gap: 70,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+});
