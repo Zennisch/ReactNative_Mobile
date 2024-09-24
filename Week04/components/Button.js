2a6ab2b0c2424d2d89d80a7f4d29e84a9935e8b1
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-24 17:39:14 +0900

    Delete Week03, Week04

--- Week04/components/Button.js
+++
@@ -1,9 +0,0 @@
-import {Text, TouchableOpacity} from "react-native";
-
-export default function Button({ title, styleButton, styleFont, onPress = () => {} }) {
-    return (
-        <TouchableOpacity style={styleButton} onPress={onPress}>
-            <Text style={styleFont}>{title}</Text>
-        </TouchableOpacity>
-    )
-}
