31fca1050678357df1c212146a3d2c32472e818e
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-30 22:42:09 +0900

    Update 31_40

--- Week01/01_50_Challenges_Newbie/31_40.js
+++ Week01/01_50_Challenges_Newbie/31_40.js
@@ -27,7 +27,7 @@ let calcSum = (numbers) => {
     let sum = 0;
     let array = numbers.split(',');
     array.forEach(element => {
-        sum += parseInt(element);
+        sum += element.toString().trim() * 1;
     })
     return sum;
 }
@@ -47,3 +47,58 @@ console.log()
 
 // 35. Create a function to convert a CSV text to a “bi-dimensional” array
 console.log('35)');
+console.log()
+
+// 36. Create a function that converts a string to an array of characters
+console.log('36)');
+let textString = "Hello";
+let stringArray = (text) => {
+    return text.split('');
+}
+console.log(stringArray(textString));
+console.log()
+
+// 37. Create a function that will convert a string in an array containing the ASCII codes of each character
+console.log('37)');
+let textASCII = "Hello";
+let stringASCII = (text) => {
+    let array = [];
+    for (let i = 0; i < text.length; i++) {
+        array.push(text.charCodeAt(i));
+    }
+    return array;
+}
+console.log(stringASCII(textASCII));
+console.log()
+
+// 38. Create a function that will convert an array containing ASCII codes in a string
+console.log('38)');
+let arrayASCII = [72, 101, 108, 108, 111];
+let arrayString = (array) => {
+    let text = '';
+    array.forEach(element => {
+        text += String.fromCharCode(element);
+    })
+    return text;
+}
+console.log(arrayString(arrayASCII));
+console.log()
+// 39. Implement the Caesar cypher
+
+// 40. Implement the bubble sort algorithm for an array of numbers
+console.log('40)');
+let arrayNumbers = [5, 3, 8, 2, 1, 4];
+let bubbleSort = (array) => {
+    let n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+console.log(bubbleSort(arrayNumbers));
