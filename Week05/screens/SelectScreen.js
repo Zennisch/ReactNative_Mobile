041393d60419121ff45aeeac39bcaae83ff4b4ec
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-30 13:03:25 +0900

    Update SelectScreen.js

--- Week05/screens/SelectScreen.js
+++ Week05/screens/SelectScreen.js
@@ -1,9 +1,71 @@
-import {Text, View} from "react-native";
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
 
-export default function SelectScreen() {
+export default function App() {
     return (
-        <View>
-            <Text>Select Screen</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, padding: 10, backgroundColor: '#EEEEEE' }}>
+            <View
+                style={{ height: '20%', flexDirection: 'row' }}>
+                <View style={{ width: '30%' }}>
+                    <Image source={require('../myassets/vs_blue.png')}
+                           style={{width: '100%', height: '100%'}}
+                           resizeMode= 'contain'
+                    />
+                </View>
+                <View style={{ width: '70%' }}>
+                    <Text>Điện thoại Vsmart Joy 3</Text>
+                    <Text>Hàng chính hãng</Text>
+                </View>
+            </View>
+            <View
+                style={{
+                    height: '80%',
+                    padding: 10,
+                    gap: 10,
+                    backgroundColor: 'lightgray',
+                }}>
+                <Text>Chọn một màu bên dưới</Text>
+                <View
+                    style={{
+                        width: '100%',
+                        height: '80%',
+                        gap: 10,
+                        alignItems: 'center',
+                    }}>
+                    <View
+                        style={{
+                            aspectRatio: 1,
+                            height: '20%',
+                            backgroundColor: '#aaaaff',
+                        }}></View>
+                    <View
+                        style={{
+                            aspectRatio: 1,
+                            height: '20%',
+                            backgroundColor: 'red',
+                        }}></View>
+                    <View
+                        style={{
+                            aspectRatio: 1,
+                            height: '20%',
+                            backgroundColor: 'black',
+                        }}></View>
+                    <View
+                        style={{
+                            aspectRatio: 1,
+                            height: '20%',
+                            backgroundColor: '#4444FF',
+                        }}></View>
+                </View>
+                <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#4444FF', height: '10%', borderRadius: 10}}>
+                    <Text style={{color: 'white'}}>XONG</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     );
 }
