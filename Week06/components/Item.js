fb5aaf7ba6203528029f35df6b495e1770c8758c
Cosmic Reina <snowy.sunny.dazing.us@gmail.com>
2024-09-30 18:40:56 +0900

    Add assets & Update

--- Week06/components/Item.js
+++ Week06/components/Item.js
@@ -1,17 +1,22 @@
-import {Text, View} from "react-native";
+import {Image, Text, View} from "react-native";
 import s from "../styles/MyStyles";
+import Button from "./Button";
 
 export default function Item({item}) {
     return (
-        <View style={[s.flex, s.flexRow, s.alignCenter, s.justifyStart, {height: 200}]}>
-            <View style={[s.w20, s.h100, s.bgRed]}>
-                <Text>{item.name}</Text>
+        <View style={[s.flex, s.flexRow, s.alignCenter, s.justifyStart, {height: 100}]}>
+            <View style={[s.w20, s.h100, s.justifyStart_alignCenter]}>
+                <Image source={item.image} style={[s.w90, s.h90]} />
             </View>
-            <View style={[s.w60, s.h100, s.bgBlue]}>
+            <View style={[s.w60, s.h100]}>
+                <Text>{item.name}</Text>
                 <Text>{item.distributor}</Text>
             </View>
-            <View style={[s.w20, s.h100, s.bgGreen]}>
-                <Text>{item.image}</Text>
+            <View style={[s.w20, s.h100, s.justifyCenter_alignCenter]}>
+                <Button title={"Chat"}
+                        styleButton={[s.bgRed, s.w90, s.h50, s.justifyCenter_alignCenter]}
+                        styleText={[s.textWhite]}
+                />
             </View>
         </View>
     )
--- Week06/screens/ItemScreen.js
+++ Week06/screens/ItemScreen.js
@@ -4,16 +4,47 @@ import Item from "../components/Item";
 export default function ItemScreen({route}) {
     const items = [
         {
-            "id": 1,
-            "name": "Item 1",
-            "distributor": "Distributor 1",
-            "image": "",
+            "id": "1",
+            "name": "Ca nấu lẩu, nấu mì mini",
+            "distributor": "Shop Devang",
+            "image": require("../myassets/ca_nau_lau.png"),
         },
         {
-            "id": 2,
-            "name": "Item 2",
-            "distributor": "Distributor 2",
-            "image": "",
+            "id": "2",
+            "name": "1KG KHÔ GÀ BƠ TỎI",
+            "distributor": "LTD Food",
+            // "image": "../myassets/ga_bo_toi.png",
+            "image": require("../myassets/ga_bo_toi.png"),
+        },
+        {
+            "id": "3",
+            "name": "Xe cần cẩu đa năng",
+            "distributor": "Thế giới đồ chơi",
+            "image": require("../myassets/xe_can_cau.png"),
+        },
+        {
+            "id": "4",
+            "name": "Đồ chơi dạng mô hình",
+            "distributor": "Thế giới đồ chơi",
+            "image": require("../myassets/do_choi_dang_mo_hinh.png"),
+        },
+        {
+            "id": "5",
+            "name": "Lãnh đạo giản đơn",
+            "distributor": "Mini Long Book",
+            "image": require("../myassets/lanh_dao_gian_don.png"),
+        },
+        {
+            "id": "6",
+            "name": "Hiểu lòng con trẻ",
+            "distributor": "Mini Long Book",
+            "image": require("../myassets/hieu_long_con_tre.png"),
+        },
+        {
+            "id": "7",
+            "name": "Donald Trump Thiên tài lãnh đạo",
+            "distributor": "Mini Long Book",
+            "image": require("../myassets/trump_1.png"),
         }];
 
     return (
@@ -22,6 +53,5 @@ export default function ItemScreen({route}) {
             renderItem={({item}) => (Item({item}))}
             keyExtractor={item => item.id.toString()}
         />
-
     )
 }
